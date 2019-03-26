cee8784d0ab1aa8d920c59a37cf90d4c318041c3
umeshbatra <umeshbatra@umeshs-air>
2019-03-26 23:46:04 +0530

    File commit

---
+++ java-5-brains/src/main/java/TestJunit.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+public class TestJunit {
+	
+	static Logger logger = Logger.getLogger(TestJunit.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("In the main class");
+
+	}
+
+}
